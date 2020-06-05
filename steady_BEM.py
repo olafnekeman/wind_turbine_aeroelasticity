9e995d3399be77c7a4989277409f4ce7bf2ede19
Peter Blom <pet.188@hotmail.com>
2020-06-05 11:24:19 +0200

    Create steady_BEM.py

---
+++ steady_BEM.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 10:41:27 2020
+"""
+import numpy as np
+import pandas as pd
+
+def CTfunction(a, glauert = False):
+    """
+    This function calculates the thrust coefficient as a function of induction factor 'a'
+    'glauert' defines if the Glauert correction for heavily loaded rotors should be used; default value is false
+    """
+    CT = np.zeros(np.shape(a))
+    CT = 4*a*(1-a)  
+    if glauert:
+        CT1=1.816;
+        a1=1-np.sqrt(CT1)/2;
+        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
+    
+    return CT
+  
+    
+def ainduction(CT):
+    """
+    This function calculates the induction factor 'a' as a function of thrust coefficient CT 
+    including Glauert's correction
+    """
+    a = np.zeros(np.shape(CT))
+    CT1=1.816;
+    CT2=2*np.sqrt(CT1)-CT1
+    a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
+    a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
+    return a
+
+def PrandtlTipRootCorrection(r_R, rootradius_R, tipradius_R, TSR, NBlades, axial_induction):
+    """
+    This function calcualte steh combined tip and root Prandtl correction at agiven radial position 'r_R' (non-dimensioned by rotor radius), 
+    given a root and tip radius (also non-dimensioned), a tip speed ratio TSR, the number lf blades NBlades and the axial induction factor
+    """
+    temp1 = -NBlades/2*(tipradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Ftip = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Ftip[np.isnan(Ftip)] = 0
+    temp1 = NBlades/2*(rootradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Froot = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Froot[np.isnan(Froot)] = 0
+    return Froot*Ftip, Ftip, Froot
+
+def loadBladeElement(vnorm, vtan, r_R, chord, twist, polar_alpha, polar_cl, polar_cd):
+    """
+    calculates the load in the blade element
+    """
+    vmag2 = vnorm**2 + vtan**2
+    inflowangle = np.arctan2(vnorm,vtan)
+    alpha = twist + inflowangle*180/np.pi
+    cl = np.interp(alpha, polar_alpha, polar_cl)
+    cd = np.interp(alpha, polar_alpha, polar_cd)
+    lift = 0.5*vmag2*cl*chord
+    drag = 0.5*vmag2*cd*chord
+    fnorm = lift*np.cos(inflowangle)+drag*np.sin(inflowangle)
+    ftan = lift*np.sin(inflowangle)-drag*np.cos(inflowangle)
+    gamma = 0.5*np.sqrt(vmag2)*cl*chord
+    return fnorm , ftan, gamma
+
+def solveStreamtube(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd ):
+    """
+    solve balance of momentum between blade element load and loading in the streamtube
+    input variables:
+    Uinf - wind speed at infinity
+    r1_R,r2_R - edges of blade element, in fraction of Radius ;
+    rootradius_R, tipradius_R - location of blade root and tip, in fraction of Radius ;
+    Radius is the rotor radius
+    Omega -rotational velocity
+    NBlades - number of blades in rotor
+    """
+    Area = np.pi*((r2_R*Radius)**2-(r1_R*Radius)**2) #  area streamtube
+    r_R = (r1_R+r2_R)/2 # centroide
+    # initiatlize variables
+    a = 0.0 # axial induction
+    aline = 0.0 # tangential induction factor
+    
+    Niterations = 1000
+    Erroriterations =0.00001 # error limit for iteration rpocess, in absolute value of induction
+    
+    for i in range(Niterations):
+        # ///////////////////////////////////////////////////////////////////////
+        # // this is the block "Calculate velocity and loads at blade element"
+        # ///////////////////////////////////////////////////////////////////////
+        Urotor = Uinf*(1-a) # axial velocity at rotor
+        Utan = (1+aline)*Omega*r_R*Radius # tangential velocity at rotor
+        # calculate loads in blade segment in 2D (N/m)
+        fnorm, ftan, gamma = loadBladeElement(Urotor, Utan, r_R,chord, twist, polar_alpha, polar_cl, polar_cd)
+        load3Daxial =fnorm*Radius*(r2_R-r1_R)*NBlades # 3D force in axial direction
+        # load3Dtan =loads[1]*Radius*(r2_R-r1_R)*NBlades # 3D force in azimuthal/tangential direction (not used here)
+      
+        # ///////////////////////////////////////////////////////////////////////
+        # //the block "Calculate velocity and loads at blade element" is done
+        # ///////////////////////////////////////////////////////////////////////
+
+        # ///////////////////////////////////////////////////////////////////////
+        # // this is the block "Calculate new estimate of axial and azimuthal induction"
+        # ///////////////////////////////////////////////////////////////////////
+        # // calculate thrust coefficient at the streamtube 
+        CT = load3Daxial/(0.5*Area*Uinf**2)
+        
+        # calculate new axial induction, accounting for Glauert's correction
+        anew =  ainduction(CT)
+        
+        # correct new axial induction with Prandtl's correction
+        Prandtl, Prandtltip, Prandtlroot = PrandtlTipRootCorrection(r_R, rootradius_R, tipradius_R, Omega*Radius/Uinf, NBlades, anew);
+        if (Prandtl < 0.0001): 
+            Prandtl = 0.0001 # avoid divide by zero
+        anew = anew/Prandtl # correct estimate of axial induction
+        a = 0.85*a+0.15*anew # for improving convergence, weigh current and previous iteration of axial induction
+
+        # calculate aximuthal induction
+        aline = ftan*NBlades/(2*np.pi*Uinf*(1-a)*Omega*2*(r_R*Radius)**2)
+        aline =aline/Prandtl # correct estimate of azimuthal induction with Prandtl's correction
+        # ///////////////////////////////////////////////////////////////////////////
+        # // end of the block "Calculate new estimate of axial and azimuthal induction"
+        # ///////////////////////////////////////////////////////////////////////
+        
+        #// test convergence of solution, by checking convergence of axial induction
+        if (np.abs(a-anew) < Erroriterations): 
+            # print("iterations")
+            # print(i)
+            break
+    if i == Niterations-1:
+        print('WARNING: BEM model did not converge within {} iterations, consider increasing iteration amount.'.format(Niterations))
+    return [a , aline, r_R, fnorm , ftan, gamma]
+
+
+class steady_BEM:
+    def __init__(self, airfoil, TipLocation_R, RootLocation_R, NBlades, Radius, Uinf, TSR, N_blade_sec, spacing = 'cosine'):
+        self.airfoil = airfoil
+        self.NBlades = NBlades
+        self.Radius = Radius
+        self.Uinf = Uinf
+        self.Omega = TSR*Uinf/Radius
+        self.N_blade_sec = N_blade_sec
+        self.RootLocation_R = RootLocation_R
+        self.TipLocation_R = TipLocation_R
+        #create the blade division
+        if spacing == 'cosine':
+            beta_cosine = np.linspace(0,np.pi,N_blade_sec+1)
+            self.r_R_dist = RootLocation_R+(TipLocation_R-RootLocation_R)/2*(1-np.cos(beta_cosine))
+        elif spacing == 'uniform':
+            self.r_R_dist = np.linspace(RootLocation_R,TipLocation_R,N_blade_sec+1)
+        else:
+            raise ValueError('Spacing method not recognized, please select either "cosine" or "uniform"')
+        
+        self.r_R_cent = np.zeros(N_blade_sec)
+        for i in range(N_blade_sec):
+            self.r_R_cent[i] = (self.r_R_dist[i]+self.r_R_dist[i+1])/2
+            
+        self.twist_no_pitch = -14*(1-self.r_R_cent)
+        self.chord_cent = 3*(1-self.r_R_cent)+1
+        
+        data1=pd.read_csv(airfoil, header=0,
+                            names = ["alfa", "cl", "cd", "cm"],  sep='\s+')
+        self.polar_alpha = data1['alfa'][:]
+        self.polar_cl = data1['cl'][:]
+        self.polar_cd = data1['cd'][:]
+            
+    def get_solution(self,pitch):
+        self.twist_cent = self.twist_no_pitch+pitch
+        results =np.zeros([self.N_blade_sec,6])
+        
+        for i in range(self.N_blade_sec):
+            results[i,:] = solveStreamtube(self.Uinf, self.r_R_dist[i], self.r_R_dist[i+1], 
+                   self.RootLocation_R, self.TipLocation_R , self.Omega, self.Radius, self.NBlades, 
+                   self.chord_cent[i], self.twist_cent[i], self.polar_alpha, self.polar_cl, self.polar_cd )
+        
+        areas = (self.r_R_dist[1:]**2-self.r_R_dist[:-1]**2)*np.pi*self.Radius**2
+        dr = (self.r_R_dist[1:]-self.r_R_dist[:-1])*self.Radius
+        CT = np.sum(dr*results[:,3]*self.NBlades/(0.5*self.Uinf**2*np.pi*self.Radius**2))
+        CP = np.sum(dr*results[:,4]*results[:,2]*self.NBlades*self.Radius*self.Omega/(0.5*self.Uinf**3*np.pi*self.Radius**2))
+        return CT, CP, results
+    
+    
+if __name__ == "__main__":
+    # define flow conditions
+    Uinf = 10 # unperturbed wind speed in m/s
+    TSR = 10 # tip speed ratio
+    Radius = 50
+    N_blade_sec = 30
+    NBlades = 3
+    
+    TipLocation_R =  1
+    RootLocation_R =  0.2
+    pitch = 2
+    
+    airfoil = 'DU_polar.txt'
+    
+    B = steady_BEM(airfoil, TipLocation_R, RootLocation_R, NBlades, Radius, Uinf, TSR, N_blade_sec)
+    CT,CP, results = B.get_solution(pitch)
+    print('CT: {}'.format(CT))
+    print('CP: {}'.format(CP))
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+        
+            
+        
+        
+        
+        
